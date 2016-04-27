c1edacff5a34f4a956673d410db995b6803a04a1
Nagy András <nagyandras95@users.noreply.github.com>
2016-04-27 22:15:10 +0200

    Finalize

---
+++ HDPCPI/Auctions Portal/Models/UserItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Auctions_Portal.Models
+{
+    public class UserItemViewModel
+    {
+        public Item UserItem { get; set; }
+
+        public Boolean First { get; set; }
+
+        public Boolean Active { get; set;}
+
+    }
+}
