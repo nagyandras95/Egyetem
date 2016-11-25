905e15aff5ba52e29b1c374640817b89913933d9
Nagy András <nagyandras95@INF.ELTE.HU>
2016-11-25 15:16:49 +0100

    Add 7th task

--- information_systems/cramhead/bead2script.sql
+++ information_systems/cramhead/bead2script.sql
@@ -231,3 +231,74 @@ ALTER TABLE Tanfolyam ADD CONSTRAINT Tanf_TanfTipus_FK FOREIGN KEY ( Tanfolyam_t
 ALTER TABLE Tanfolyam ADD CONSTRAINT Tanfolyam_Oktato_FK FOREIGN KEY ( Oktato_oktatoAzon ) REFERENCES Oktato ( oktatoAzon ) ;
 
 ALTER TABLE Tanfolyam ADD CONSTRAINT Tanfolyam_Szalloda_FK FOREIGN KEY ( Szalloda_szallodaAzon ) REFERENCES Szalloda ( szallodaAzon ) ;
+
+-- 7. feladat
+--/1 két különböző seassionben kell lefuttani, először lokkolni kell az egyes táblákat az elején..
+create table dolgozo1 as select * from nikovits.dolgozo;
+create table dolgozo2 as select * from nikovits.dolgozo;
+
+LOCK TABLE dolgozo1 IN EXCLUSIVE MODE;
+set serveroutput on
+begin
+update dolgozo1 
+set FIZETES = FIZETES + 1;
+LOCK TABLE dolgozo2 IN EXCLUSIVE MODE;
+update dolgozo2
+set FIZETES = FIZETES + 1;
+end;
+
+LOCK TABLE dolgozo2 IN EXCLUSIVE MODE;
+set serveroutput on
+begin
+update dolgozo1 
+set FIZETES = FIZETES + 1;
+LOCK TABLE dolgozo1 IN EXCLUSIVE MODE;
+update dolgozo2
+set FIZETES = FIZETES + 1;
+end;
+
+--/2
+create or replace procedure not_serial is
+fiz INTEGER;
+begin
+    set transaction isolation level SERIALIZABLE; 
+    update dolgozo1 
+    set FIZETES = FIZETES + 1;
+    commit;
+  begin  
+   set transaction isolation level SERIALIZABLE; 
+    update dolgozo1 
+    set FIZETES = FIZETES + 1;
+   commit;
+  end;
+  
+end;
+
+--/3
+create or replace procedure lock_test(p1 varchar2, p2 varchar2) as
+begin
+      CASE p1
+          WHEN 'RS' THEN LOCK TABLE dolgozo1 IN ROW SHARE MODE;
+          WHEN 'RX' THEN LOCK TABLE dolgozo1 IN ROW SHARE MODE;
+          WHEN 'S' THEN LOCK TABLE dolgozo1 IN SHARE MODE;
+          WHEN 'SRX' THEN LOCK TABLE dolgozo1 IN SHARE ROW EXCLUSIVE MODE;
+          WHEN 'x' THEN LOCK TABLE dolgozo1 IN EXCLUSIVE MODE;
+      END CASE;
+        begin
+            CASE p2
+          WHEN 'RS' THEN LOCK TABLE dolgozo1 IN ROW SHARE MODE NOWAIT;
+          WHEN 'RX' THEN LOCK TABLE dolgozo1 IN ROW SHARE MODE NOWAIT;
+          WHEN 'S' THEN LOCK TABLE dolgozo1 IN SHARE MODE NOWAIT;
+          WHEN 'SRX' THEN LOCK TABLE dolgozo1 IN SHARE ROW EXCLUSIVE MODE NOWAIT;
+          WHEN 'x' THEN LOCK TABLE dolgozo1 IN EXCLUSIVE MODE NOWAIT;
+        END CASE;
+      end;
+      DBMS_OUTPUT.put_line('IGEN');
+      COMMIT;
+
+      
+exception
+   WHEN OTHERS THEN 
+      DBMS_OUTPUT.put_line('NEM');
+      ROLLBACK;
+end;
