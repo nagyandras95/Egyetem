d57dcf4432a98386f2c85244c7aaf5f9632d54ce
Nagy András <nagyandras95@inf.elte.hu>
2018-03-14 10:05:19 +0100

    Add assoc plan alternatives

---
+++ efop/assoc_alt.cpp
@@ -0,0 +1,134 @@
+//
+
+#include "stdafx.h"
+#include <assert.h>
+#include <map>
+#include <list>
+
+
+
+template<class FirstClassType, class FirstClassRole, class SecondClassType, class SecondClassRole>
+class AssociationNN {
+public:
+	void link (FirstClassType* first, FirstClassRole, SecondClassType* second, SecondClassRole) {
+		LeftRoleTable[first].push_back (second);
+		RigthRoleTable[second].push_back (first);
+	}
+
+	void link (SecondClassType* first, SecondClassRole, FirstClassType* second, FirstClassRole) {
+		RigthRoleTable[first].push_back (second);
+		LeftRoleTable[second].push_back (first);
+	}
+
+	std::list<SecondClassType*> get (FirstClassType* left, FirstClassRole) {
+		return LeftRoleTable[left];
+	}
+
+	std::list<FirstClassType*> get (SecondClassType* rigth, SecondClassRole) {
+		return RigthRoleTable[rigth];
+	}
+
+private:
+	std::map<typename FirstClassType*, std::list<SecondClassType*>> LeftRoleTable;
+	std::map<typename SecondClassType*, std::list<FirstClassType*>> RigthRoleTable;
+	
+};
+
+
+template<class FirstClassRoleType, class SecondClassRoleType>
+class AssociationNNAlt {
+public:
+	template<typename FirstType, typename SecondType>
+	void link (typename FirstType::RoleType* first, typename SecondType::RoleType* second) {
+		assert (false);
+	}
+
+	template<>
+	void link<typename FirstClassRoleType, SecondClassRoleType> (typename FirstClassRoleType::RoleType* first, typename SecondClassRoleType::RoleType* second) {
+		LeftRoleTable[first].push_back (second);
+		RigthRoleTable[second].push_back (first);
+	}
+
+	template<>
+	void link<typename SecondClassRoleType, typename FirstClassRoleType> (typename SecondClassRoleType::RoleType* first, typename FirstClassRoleType::RoleType* second) {
+		RigthRoleTable[first].push_back (second);
+		LeftRoleTable[second].push_back (first);
+	}
+
+	template<typename ParamType>
+	auto get (typename ParamType::RoleType* o) {
+		assert (false);
+	}
+
+	template<>
+	auto get<typename FirstClassRoleType> (typename FirstClassRoleType::RoleType* o) {
+		return LeftRoleTable[o];
+	}
+
+	template<>
+	auto get<typename SecondClassRoleType> (typename SecondClassRoleType::RoleType* o) {
+		return RigthRoleTable[o];
+	}
+
+private:
+	std::map<typename FirstClassRoleType::RoleType*, std::list<typename SecondClassRoleType::RoleType*>> LeftRoleTable;
+	std::map<typename SecondClassRoleType::RoleType*, std::list<typename FirstClassRoleType::RoleType*>> RigthRoleTable;
+
+};
+
+
+class A {
+public:
+	int id;
+};
+
+
+enum class A1 {
+	Selector
+};
+
+enum class A2 {
+	Selector
+};
+
+struct ARoleStruct {
+	struct a1 {
+		typedef A RoleType;
+	};
+	struct a2 {
+		typedef A RoleType;
+	};
+
+};
+
+int main()
+{
+	AssociationNN<A, A1, A, A2> aAssoc;
+	//vs
+	AssociationNNAlt<ARoleStruct::a1, ARoleStruct::a2 > altAssoc;
+
+	A a1;
+	a1.id = 1;
+	A a2;
+	a2.id = 2;
+
+
+	aAssoc.link (&a1, A1::Selector, &a2, A2::Selector);
+	altAssoc.link<ARoleStruct::a1, ARoleStruct::a2> (&a1, &a2);
+
+	std::list<A*> rigthObjectsA1 = aAssoc.get (&a1, A1::Selector);
+	std::list<A*> leftObjectsA1 = aAssoc.get (&a1, A2::Selector);
+	std::list<A*> rigthObjectsA2 = aAssoc.get (&a2, A1::Selector);
+	std::list<A*> leftObjectsA2 = aAssoc.get (&a2, A2::Selector);
+
+	
+	std::list<A*> rigthObjectsA1alt = altAssoc.get<ARoleStruct::a1> (&a1);
+	std::list<A*> leftObjectsA1alt = altAssoc.get<ARoleStruct::a2> (&a1);
+	std::list<A*> rigthObjectsA2alt = altAssoc.get<ARoleStruct::a1> (&a2);
+	std::list<A*> leftObjectsA2alt = altAssoc.get<ARoleStruct::a2> (&a2);
+	
+
+
+    return 0;
+}
+
